972e9caecd4e58ff215e34333bd9712882db669e
Edgar <edguitian@imagine800.es>
2023-12-15 10:45:22 +0100

    Initial Commit

---
+++ RickAndMortyCleanArquitecture/RickAndMortyCleanArquitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyCleanArquitectureApp.swift
+//  RickAndMortyCleanArquitecture
+//
+//  Created by Edgar Guitian Rey on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyCleanArquitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
